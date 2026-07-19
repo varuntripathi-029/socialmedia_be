745089fbdeda9ec48ce6fc7f50f55c8c7ec92931
varuntripathi-029 <159700481+varuntripathi-029@users.noreply.github.com>
2026-07-20 00:55:03 +0530

    add pagination size constants and clamp helper

--- src/main/java/com/socialmedia/app/util/Constants.java
+++ src/main/java/com/socialmedia/app/util/Constants.java
@@ -5,6 +5,16 @@ public class Constants {
     public static final String BEARER_PREFIX = "Bearer ";
     public static final int JWT_EXPIRATION_DAYS = 1;
 
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final int MAX_PAGE_SIZE = 50;
+
+    public static int clampPageSize(int requestedSize) {
+        if (requestedSize <= 0) {
+            return DEFAULT_PAGE_SIZE;
+        }
+        return Math.min(requestedSize, MAX_PAGE_SIZE);
+    }
+
     private Constants() {
         // Private constructor to prevent instantiation
     }
